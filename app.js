048b8758b1b2564637ff48c17cbc5688d892ef5f
Joao.Cordeiro <joaocordeiro2134@gmail.com>
2023-03-08 08:43:53 -0300

    ajuste para subir em produção

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ import { serverHttp } from "./backend/http.js";
 
 import "./backend/websocket.js"
 
-serverHttp.listen(3000, () => {
+serverHttp.listen(process.env.PORT || 3000, () => {
     console.log('App is running at port 3000...');
 });
